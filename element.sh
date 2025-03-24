aecfb713f17f18cbdc6ea2b7750d73c75d3ad409
Doğukan Demir <senseizeus190701@gmail.com>
2025-03-24 11:44:27 +0000

    chore: Set up element.sh script

--- element.sh
+++ element.sh
@@ -2,13 +2,13 @@ PSQL="psql -X --username=freecodecamp --dbname=periodic_table --tuples-only -c"
 ELEMENT=$1
 
 if [[ -z $ELEMENT ]]; then
-  # echo "Please provide an element as an argument."
+  echo "Please provide an element as an argument."
 else
   
   if [[ ! $ELEMENT =~ ^[0-9]+$ ]]; then
     
     NAME_LENGTH=$(echo -n "$ELEMENT" | wc -m)
-    if [[ $NAME_LENGTH -gt 2 ]]; then
+    # if [[ $NAME_LENGTH -gt 2 ]]; then
       
       QUERY_RESULT=$($PSQL "SELECT * FROM elements INNER JOIN properties USING(atomic_number) INNER JOIN types USING(type_id) WHERE name='$ELEMENT'")
       
